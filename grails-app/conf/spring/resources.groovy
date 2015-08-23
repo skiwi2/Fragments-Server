fc742e5febe74d85d27c9e3cf6f3baaff927e3c1
skiwi <frank_skiwi@hotmail.com>
2015-08-23 17:20:59 +0200

    Made WebSocket server host and port configurable in application.yml.

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -2,5 +2,5 @@ import com.skiwi.fragments.server.FragmentsServer
 
 // Place your Spring DSL code here
 beans = {
-    fragmentsServer(FragmentsServer, "localhost", "6644") { }
+    fragmentsServer(FragmentsServer, '${webSocketServer.host}', '${webSocketServer.port}') { }
 }
